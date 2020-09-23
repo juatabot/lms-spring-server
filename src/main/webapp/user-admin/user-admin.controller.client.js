6c9634c5b7293bf611440551b4003c70856e2690
juatabot <hengjuat@gmail.com>
2020-09-23 14:36:43 -0400

    Render users from dictionary

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -20,6 +20,7 @@ let users = [
 ];
 
 let $template;
+let tbody;
 
 
 function renderUsers(users) {
@@ -31,13 +32,24 @@ function renderUsers(users) {
         const role = usr.role;
 
         const $trClone = $template.clone();
+        $trClone.removeClass("wbdv-hidden")
+
+        const $username = $trClone.find(".wbdv-username")
+        $username.html(username)
+        const $firstName = $trClone.find(".wbdv-first-name")
+        $firstName.html(fName)
+        const $lastName = $trClone.find(".wbdv-last-name")
+        $lastName.html(lName)
+        // const $removeBtn = $trClone.find(".wbdv-remove")
+
+        tbody.append($trClone)
     })
 }
 
 function init() {
     $template = jQuery(".wbdv-template");
+    tbody = $("tbody.wbdv-tbody")
     renderUsers(users);
-
 }
 
 jQuery(init)
