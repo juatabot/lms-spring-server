f638367f51f2a84c8b24f1e3e5bed097039cf53b
juatabot <hengjuat@gmail.com>
2020-09-24 12:46:22 -0400

    Add jQuery edit and render to user-admin

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -22,18 +22,42 @@ let users = [
 let $template;
 let tbody;
 
-const onclickEventHandler = () => {
-    alert("heading clicked")
+function deleteUser(event) {
+    // remove table row
+    const deleteBtn = event.currentTarget;
+    const $deleteBtn = $(deleteBtn);
+    const $tr = $deleteBtn.closest("tr");
+    $tr.remove();
 }
 
-const deleteUser = (event) => {
-    const deleteBtn = event.currentTarget
-    const $deleteBtn = $(deleteBtn)
-    const $parent = $deleteBtn.closest("tr")
-    console.log($parent)
-    $parent.remove()
-}
+function editForm(event) {
+    // populate edit form with table row data
+    const editBtn = event.currentTarget;
+    const $editBtn = $(editBtn);
+    const $tr = $editBtn.closest("tr");
 
+    const username = $tr.find(".wbdv-username").text();
+    const firstName = $tr.find(".wbdv-first-name").text();
+    const lastName = $tr.find(".wbdv-last-name").text();
+    const role = $tr.find(".wbdv-role").text();
+
+    const $inputForm = $(".wbdv-form");
+
+    $inputForm.find("#usernameFld").val(username);
+    $inputForm.find("#firstNameFld").val(firstName);
+    $inputForm.find("#lastNameFld").val(lastName);
+
+    const $selectForm = $inputForm.find("#roleFld");
+
+    const $selected = $selectForm.find(role);
+    console.log($selected);
+    $inputForm.find("#roleFld")
+        .find('role')
+        .remove()
+        .end()
+        .append('<option selected value="role"></option>'.replace("role", role))
+        .val(role);
+}
 
 function renderUsers(users) {
     users.forEach(usr => {
@@ -44,25 +68,31 @@ function renderUsers(users) {
         const role = usr.role;
 
         const $trClone = $template.clone();
-        $trClone.removeClass("wbdv-hidden")
+        $trClone.removeClass("wbdv-hidden");
 
-        const $username = $trClone.find(".wbdv-username")
-        $username.html(username)
-        const $firstName = $trClone.find(".wbdv-first-name")
-        $firstName.html(fName)
-        const $lastName = $trClone.find(".wbdv-last-name")
-        $lastName.html(lName)
-        const $removeBtn = $trClone.find(".wbdv-remove")
-        $removeBtn.click((event) => deleteUser(event))
+        const $username = $trClone.find(".wbdv-username");
+        $username.html(username);
+        const $firstName = $trClone.find(".wbdv-first-name");
+        $firstName.html(fName);
+        const $lastName = $trClone.find(".wbdv-last-name");
+        $lastName.html(lName);
+        const $role = $trClone.find(".wbdv-role");
+        console.log($role);
+        $role.html(role);
 
-        tbody.append($trClone)
+        const $removeBtn = $trClone.find(".wbdv-remove");
+        $removeBtn.click((event) => deleteUser(event));
+        const $editBtn = $trClone.find(".wbdv-update");
+        $editBtn.click((event) => editForm(event));
+
+        tbody.append($trClone);
     })
 }
 
 function init() {
     $template = jQuery(".wbdv-template");
-    tbody = $("tbody.wbdv-tbody")
+    tbody = $("tbody.wbdv-tbody");
     renderUsers(users);
 }
 
-jQuery(init)
+$(init)
